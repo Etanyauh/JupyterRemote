3a4984b5424353435f055d372b085b505855e38c
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-12-13 09:33:02 -0500

    Initialized Styles Created nav file

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Navigation from './Nav'
 
 class Header extends Component {
     render() {
         return (
             <header>
+                <Navigation />
                 <p>The header</p>
             </header>
         )
---
+++ src/components/Header/Nav.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props)
+        this.states = {
+            // Gets the user token
+            user_token: window.sessionStorage.getItem('jwtToken'),
+
+            // Links for unauthorized and authorized
+            no_auth_links: [
+                { text: "Home", url: "/" },
+                { text: "About", url: "/about" },
+                { text: "Login", url: "/login" },
+            ],
+            auth_links: [
+                { text: 'Home', url: '/' },
+                { text: 'About', url: '/about' },
+                { text: 'Notebooks', url: '/notebooks' },
+            ]
+        }
+
+        this.navLinks = this.navLinks.bind(this)
+    }
+
+    render() {
+        return (
+            <nav className="navigation">
+                <h3>Jupyter Remote</h3>
+                <ul className="nav-items">
+                    {this.navLinks()}
+                </ul>
+            </nav>
+        )
+    }
+
+    navLinks() {
+        if (this.states.user_token) {
+            return this.authLinks()
+        }
+
+        return this.noAuthLinks()
+    }
+
+    authLinks() {
+        return this.states.auth_links.map((val, key) => {
+            return (
+                <li key={key}>
+                    <Link to={val.url}>{val.text}</Link>
+                </li>
+            )
+        })
+    }
+
+    noAuthLinks() {
+        return this.states.no_auth_links.map((val, key) => {
+            return (
+                <li key={key}>
+                    <Link to={val.url}>{val.text}</Link>
+                </li>
+            )
+        })
+    }
+}
+
+export default Navigation
